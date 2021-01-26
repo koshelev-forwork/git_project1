30fe3a2bd6fb766f581e4343714b6a903299e62a
koshelev-forwork <koshelev_forwork@mail.ru>
2021-01-27 03:36:24 +0700

    Git и желтые окружности

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import sys
-
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from PyQt5.QtGui import QPainter, QColor, QBrush
+import random
 from PyQt5 import uic, QtGui
+import sys
 
 
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         uic.loadUi('UI.ui', self)
-        self.initUi()
-    #
-    # def initUI(self):
+        self.crug.clicked.connect(self.onClicked)
+        self.setWindowTitle('Желтые круги')
+        self.show()
+        self.flag = False
+
+    def onClicked(self):
+        self.flag = True
+        self.update()
+
+    def paintEvent(self, e):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawEll(qp)
+            qp.end()
+
+    def drawEll(self, qp):
+        d = random.randint(10, 600)
+        qp.setBrush(QColor(255, 255, 0))
+        # qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        qp.drawEllipse(random.randint(10, 600), random.randint(10, 600), d, d)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+    ex = Window()
+    sys.exit(app.exec_())
